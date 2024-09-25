664a76224bb27c6a9864b4f39a7d345fad44a293
santosh1437 <santoshjenasanta@gmail.com>
2024-09-25 15:30:36 +0530

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,7 +17,7 @@ const routes: Routes = [
   {
     path: 'dashboard',
     component: AdminPageComponent,
-    canActivate: [MyGuardGuard],
+    // canActivate: [MyGuardGuard],
   },
   {
     path:'zoom1',
